47130e7ecc2f6922721853105d2b2e86e47432bb
Hyeokmin Kwon <khmzxc@naver.com>
2026-04-27 00:45:54 +0900

    fix: 용어 수정

--- src/pages/Conduct.jsx
+++ src/pages/Conduct.jsx
@@ -7,7 +7,7 @@ function Conduct() {
         <h1 className="about-heading">Code of Conduct</h1>
 
         <p className="about-text">
-          PyCon Busan은 모든 참가자가 서로 존중하며 안전하게 참여할 수 있는
+          파이콘 부산은 모든 참가자가 서로 존중하며 안전하게 참여할 수 있는
           환경을 만들기 위해 행동 강령을 시행합니다.
         </p>
       </div>
