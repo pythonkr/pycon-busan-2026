99f3812496bceef104f6b736a04a1f6f60621d61
Junwon Lee <cpprhtn@naver.com>
2026-05-04 23:57:22 +0900

    feat: 준비위원회 이미지 추가

--- src/data/team.js
+++ src/data/team.js
@@ -1,17 +1,25 @@
+import kwonImage from '../images/organizer/권혁민.png';
+import baeImage from '../images/organizer/배권한.png';
+import leeImage from '../images/organizer/이준원.jpeg';
+import janImage from '../images/organizer/잔부부.png';
+import joImage from '../images/organizer/조성수.jpeg';
+import choImage from '../images/organizer/조현진.jpeg';
+import juImage from '../images/organizer/주환석.jpg';
+
 export const team = [
   {
     id: 1,
     name: "권혁민",
     role: "committeeRole",
     org: "",
-    image: null
+    image: kwonImage
   },
   {
     id: 2,
     name: "배권한",
     role: "committeeRole",
     org: "",
-    image: null
+    image: baeImage
   },
   {
     id: 3,
@@ -25,34 +33,34 @@ export const team = [
     name: "이준원",
     role: "committeeRole",
     org: "",
-    image: null
+    image: leeImage
   },
   {
     id: 5,
     name: "잔부부",
     role: "committeeRole",
     org: "",
-    image: null
+    image: janImage
   },
   {
     id: 6,
     name: "조성수",
     role: "committeeRole",
     org: "",
-    image: null
+    image: joImage
   },
   {
     id: 7,
     name: "조현진",
     role: "committeeRole",
     org: "",
-    image: null
+    image: choImage
   },
   {
     id: 8,
     name: "주환석",
     role: "committeeRole",
     org: "",
-    image: null
+    image: juImage
   }
 ]
